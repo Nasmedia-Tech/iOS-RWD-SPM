b6ea245e68f52295fe30adfe004477a89e6009a4
seony42 <seony42@nasmedia.co.kr>
2025-10-14 16:13:27 +0900

    chore: v1.8.2-release.4

--- Package.swift
+++ Package.swift
@@ -10,24 +10,22 @@ let package = Package(
             name: "NStationOfferwall",
             targets: ["iOS_RWD_SPM"]),
     ],
-    dependencies: [
-        // Kingfisher SDK
-        .package(
-            url: "https://github.com/onevcat/Kingfisher.git",
-            .upToNextMajor(from: "7.0.0")
-        )
-    ],
     targets: [
         .binaryTarget(
             name: "NStationOfferwallBinary",
             url: "https://github.com/Nasmedia-Tech/iOS-NStationDownload/raw/main/NStationOfferwall1.8.2.xcframework.zip",
             checksum: "f97190ef62bf9749ccec8203480a88352f69fd1b8ebc509a33276fb96744d597"
         ),
+        .binaryTarget(
+            name: "Kingfisher",
+            url: "https://github.com/Nasmedia-Tech/iOS-NStationDownload/raw/main/Kingfisher7.12.0-release.xcframework.zip",
+            checksum: "12ae2e990d93d47ddc3fe3664ce7dd0ee57dfa9f34ed5cd0813f68b62d1df7e9"
+        ),
         .target(
             name: "iOS_RWD_SPM",
             dependencies: [
                 "NStationOfferwallBinary",
-                .product(name: "Kingfisher", package: "kingfisher")
+                "Kingfisher"
             ],
             path: "Sources/iOS-RWD-SPM"
         )
