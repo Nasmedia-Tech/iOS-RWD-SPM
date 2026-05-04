924ba16b10734e9f4fa560e6494a1493ec47116b
kkeman <kkeman11@gmail.com>
2026-05-04 18:47:00 +0900

    refactor: Replace Kingfisher binary target with official SPM source dependency

--- Package.swift
+++ Package.swift
@@ -10,22 +10,20 @@ let package = Package(
             name: "NStationOfferwall",
             targets: ["iOS_RWD_SPM"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/onevcat/Kingfisher", from: "7.0.0")
+    ],
     targets: [
         .binaryTarget(
             name: "NStationOfferwallBinary",
             url: "https://github.com/Nasmedia-Tech/iOS-NStationDownload/releases/download/v1.9.9/NStationOfferwall1.9.9.xcframework.zip",
             checksum: "cc6b3691cd01f57513f800b2af4b84e4d5cd31d57b7c76030934bbcc00ffdfdc"
         ),
-        .binaryTarget(
-            name: "Kingfisher",
-            url: "https://github.com/Nasmedia-Tech/iOS-NStationDownload/raw/main/Kingfisher7.12.0-release.xcframework.zip",
-            checksum: "12ae2e990d93d47ddc3fe3664ce7dd0ee57dfa9f34ed5cd0813f68b62d1df7e9"
-        ),
         .target(
             name: "iOS_RWD_SPM",
             dependencies: [
                 "NStationOfferwallBinary",
-                "Kingfisher"
+                .product(name: "Kingfisher", package: "Kingfisher")
             ],
             path: "Sources/iOS-RWD-SPM"
         )
